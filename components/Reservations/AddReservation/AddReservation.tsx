62a050d6def262ac1ce5d5696a4637e0a1371053
neel-furation <neel.narwadkar@furation.tech>
2023-08-28 12:07:26 +0530

    created add reservation modal

--- components/Reservations/AddReservation/AddReservation.tsx
+++ components/Reservations/AddReservation/AddReservation.tsx
@@ -1,28 +1,56 @@
 "use client";
 
 import React, { useState } from "react";
+import "./style.css";
+import { GMSInput } from "@/components";
 
 const AddReservation = () => {
   const [isModalOpen, setModalOpen] = useState(false);
 
+  const btns = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
   return (
     <>
       <button onClick={() => setModalOpen(true)}>Open Modal</button>
-      {isModalOpen ? (
-        <div className="fixed inset-0 flex items-center justify-center z-50">
-          <div className="absolute inset-0 bg-black opacity-50"></div>
-          <div className="bg-white p-6 rounded shadow-lg z-10">
-            <h2 className="text-xl font-semibold mb-4">Modal Title</h2>
-            <p className="mb-4">Modal content goes here.</p>
-            <button
-              className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-              onClick={() => setModalOpen(false)}
-            >
-              Close
-            </button>
+      {isModalOpen && (
+        <div className="fixed inset-0 flex items-center justify-center z-50 font-Lato">
+          <div className="fixed inset-0 bg-gray-800 opacity-50"></div>
+          <div className="bg-white w-1/2 relative">
+            <div className="py-3 px-3 bg-purple-900 flex justify-between">
+              <p className=" text-white">ADD RESERVATION</p>
+              <button
+                className="text-white"
+                onClick={() => setModalOpen(false)}
+              >
+                X
+              </button>
+            </div>
+
+            {/* Heading 1 */}
+            <div className="py-5 px-3 modal_heading">
+              <p>Availability</p>
+            </div>
+
+            {/* Heading 1 content */}
+            <div className="py-3">
+              <div className="px-3">
+                <div className="grid grid-cols-4 grid-flow-row gap-4">
+                  <div>
+                    <GMSInput label="Date" type="date" />
+                  </div>
+                </div>
+
+                {/* Guest button group */}
+                <div className="flex">
+                  {btns.map((btn, index) => (
+                    <button className="w-10 bg-purple-600">{btn}</button>
+                  ))}
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-      ) : null}
+      )}
     </>
   );
 };
