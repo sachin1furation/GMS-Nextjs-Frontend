c5e4d20988ea1864568ae64fd5bd82a08847648d
sachin <sachin.yadav@furation.tech>
2023-08-29 16:49:38 +0530

    next wrapup

--- components/GlobalComponents/GMSButton/GMSButton.tsx
+++ components/GlobalComponents/GMSButton/GMSButton.tsx
@@ -4,7 +4,7 @@ const GMSButton = ({ content = "add" }) => {
   return (
     <div>
       <button
-        className="bg-gradient-to-r from-purple-600 to-indigo-700 text-white font-medium text-lg leading-[19.36px] px-5   min-w-[15px]    h-[40px] rounded-md shadow-md hover:shadow-lg  font-inter"
+        className="bg-gradient-to-r from-purple-600 to-indigo-700 text-white font-medium text-lg leading-[19.36px] px-5   min-w-[15px]    min-h-[38px] rounded-md shadow-md hover:shadow-lg  font-inter"
       >
         {content}
       </button>
