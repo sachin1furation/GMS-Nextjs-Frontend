448ea21f88069b0737777d29ae339fe804f0ee1b
neel-furation <neel.narwadkar@furation.tech>
2023-08-30 12:47:24 +0530

    changes in logical part

--- components/Reservations/AddReservation/ClientSection.tsx
+++ components/Reservations/AddReservation/ClientSection.tsx
@@ -1,30 +1,58 @@
 "use client";
 
-import { AddNewClientForm, GMSInput, PopupCard } from "@/components";
+import { AddNewClientForm, PopupCard } from "@/components";
 import React, { useState } from "react";
 import "./style.css";
-import Image from "next/image";
-import vercel from "../../../public/vercel.svg";
 
 const ClientSection: React.FC = () => {
-  const [client, setClient] = useState("a");
+  const [client, setClient] = useState("");
+  const [clientForm, setClientForm] = useState(false);
+
+  const displayForm = () => {
+    setClientForm(true);
+  };
+
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setClient(e.target.value);
+  };
+
+  const clientData = [
+    {
+      name: "Neel Narwadkar",
+      phone: "9846574635",
+      email: "neel@gmail.com",
+      net_spend: 20000,
+      avg_visit: 8000,
+      avg_cover: 4000,
+      visits: 4,
+      cancels: 2,
+      no_shows: 0,
+    },
+  ];
 
   return (
     <>
       <div className="font-Lato">
-        <div className="px-3 py-3 font-Lato">
-          <label className="text-xs">Name, Email, Phone</label>
-          <div className="flex border border-gray-300 rounded-md">
-            <input type="text" className="flex-1 h-9" />
-            <button
-              className={`${
-                client !== "" ? "border-l" : "hidden"
-              } px-2 text-xs btn_gradient font-medium`}
-            >
-              Add as New Client
-            </button>
+        {!clientForm && (
+          <div className="px-3 py-3 font-Lato">
+            <label className="text-xs">Name, Email, Phone</label>
+            <div className="flex border border-gray-300 rounded-md">
+              <input
+                type="text"
+                className="flex-1 h-9"
+                onChange={handleInput}
+              />
+              <button
+                onClick={displayForm}
+                className={`${
+                  client !== "" ? "border-l" : "hidden"
+                } px-2 text-xs btn_gradient font-medium`}
+              >
+                Add as New Client
+              </button>
+            </div>
           </div>
-        </div>
+        )}
 
         {client === "" && (
           <p className="text-xs text-center grey_text py-2">
@@ -32,8 +60,12 @@ const ClientSection: React.FC = () => {
           </p>
         )}
 
-        {client !== "" && <PopupCard />}
-        <AddNewClientForm />
+        {client !== "" &&
+          !clientForm &&
+          clientData.map((data, index) => (
+            <PopupCard data={data} key={index} />
+          ))}
+        {clientForm && <AddNewClientForm />}
       </div>
     </>
   );
